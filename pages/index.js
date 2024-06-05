2aa63047ee2b096039a6bc0cab7022572f42a49f
zqz <qingzhe.zhou@yingxiong.com>
2024-06-05 13:37:28 +0800

    [feat] Adjust main page.

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ export default function Home({ allPostsData }) {
   console.log(allPostsData)
   return (
     <Layout home>
-      <Head>…</Head>
-      <section className={utilStyles.headingMd}>…</section>
+      {/* <Head>…</Head> */}
+      {/* <section className={utilStyles.headingMd}>…</section> */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Blogs</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
