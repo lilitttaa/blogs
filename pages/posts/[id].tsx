3fb833c72cc20581a4c50bf70a0b1065b6e52ae2
zqz <qingzhe.zhou@yingxiong.com>
2024-06-13 18:45:25 +0800

    [feat] Add react-markdown example

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Date from '../../components/date'
-
+import Markdown from 'react-markdown'
 import utilStyles from '../../styles/utils.module.css'
 import { GetStaticPaths, GetStaticProps } from 'next'
 
@@ -13,6 +13,19 @@ export default function Post({ postData }: {
     contentHtml: string
   }
 }) {
+
+  const markdownTestStr = `
+  # This is a header
+  And this is a paragraph
+  \`\`\`cpp
+  #include <iostream>
+  using namespace std;
+  int main() {
+    cout << "Hello World!";
+    return 0;
+  }
+  \`\`\`
+`
   return (
     <Layout>
       <Head>
@@ -23,7 +36,11 @@ export default function Post({ postData }: {
         <div className={utilStyles.lightText}>
           {/* <Date dateString={postData.date} /> */}
         </div>
+        {/* <div id='md'>
+        <Markdown >{markdownTestStr}</Markdown>
+        </div> */}
         <div className={utilStyles.md} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        
       </article>
     </Layout>
   )
